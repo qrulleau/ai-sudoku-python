dc3f938293f66babe5ecfb70fead405bdffa78f6
qrulleau <quentin.rulleau@yahoo.com>
2021-12-23 16:55:12 +0100

    ADD: grid system done for sudoku

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import random
 
-print("hello")
 
-def writeGridSudoku():
-    i = 0
-    tableau = []
-    while i < 9:
-        tableau.append(random.randint(0, 9))
-        i+=1
-    print(tableau)
 
-def mainTableau():
+def printSudoku(array):
+    for line in array:
+        for i in range(9):
+            if i % 3 == 0:
+                print("|", end="")
+            print(line[i], end="")
+        print("|")
+
+def lineOfMain():
+    line = []
+    for i in range(9):
+        line.append(random.randint(0, 9))
+    return line
+
+def mainArray():
     i = 0
     mainGrid = []
     for i in range(9):
-        pieceOfGrid = []
-        for i in range(9):
-            pieceOfGrid.append(random.randint(0, 9))
-        mainGrid.append(pieceOfGrid)
-    print(mainGrid)
+        mainGrid.append(lineOfMain())
+    printSudoku(mainGrid)
 
-mainTableau()
+
+
+
+
+mainArray()
